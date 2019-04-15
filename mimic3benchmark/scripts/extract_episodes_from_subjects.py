a35f0627e55138d6077b567f31fbd3c189bca4b2
Oguz Serbetci <oguz.serbetci@gmail.com>
2019-04-15 11:37:31 +0200

    Hotfix

--- mimic3benchmark/scripts/extract_episodes_from_subjects.py
+++ mimic3benchmark/scripts/extract_episodes_from_subjects.py
@@ -25,7 +25,7 @@ parser.add_argument('--reference_range_file', type=str,
 parser.add_argument('--verbose', '-v', type=int, help='Level of verbosity in output.', default=1)
 args, _ = parser.parse_known_args()
 
-d_items = pd.read_csv(args.ditems + '/D_ITEMS.csv')
+d_items = pd.read_csv(args.mimic + '/D_ITEMS.csv')
 d_labitems = pd.read_csv(args.ditems + '/D_LABITEMS.csv')
 
 var_map = read_itemid_to_variable_map(args.variable_map_file)
